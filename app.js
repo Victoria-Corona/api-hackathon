577f89a7a5346d6bdfe01a1f44fc435be805b882
Victoria Corona <corona.victoria@hotmail.com>
2020-05-12 16:29:54 -0700

    new file added

---
+++ app.js
@@ -0,0 +1,26 @@
+class App {
+constructor (){
+  this.logResult = this.logResult.bind(this);
+  this.logError = this.logError.bind(this);
+}
+logResult(result) {
+  console.log("Success!", result)
+}
+
+logError(err) {
+  console.error("Failure!", err)
+}
+
+getImage (){
+$.ajax({
+  method: "GET",
+  // headers: {
+
+  // } unless it SAYS that you need one, don't put one,
+  url: "https://pixabay.com/api/?key=" + pixabayAPIKey + "&q=nevada+tree&category=nature&image_type=photo",
+  success: logResult,
+  error: logError
+})
+}
+
+}
