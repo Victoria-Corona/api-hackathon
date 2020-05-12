8694828600cada5dc6af3110753b7b20716deaa2
Victoria Corona <corona.victoria@hotmail.com>
2020-05-12 15:10:16 -0700

    Files in correct folders, understanding google maps

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $.ajax({
   // headers: {
   //   "Access-Token": "16500842-cbac95fc55267c85b5065aa9e"
   // } unless it SAYS that you need one, don't put one,
-  url: "https://pixabay.com/api/?key=16500842-cbac95fc55267c85b5065aa9e&q=yellow+flowers&image_type=photo",
+  url: "https://pixabay.com/api/?key=" + pixabayAPIKey +"&q=nevada+tree&category=nature&image_type=photo",
 
   success: logResult,
   error: logError
