8e7ad1d4588bafa912fdc8d1fba47ca2f8190e48
Victoria Corona <corona.victoria@hotmail.com>
2020-05-13 14:43:02 -0700

    fresh start

--- components/app.js
+++ components/app.js
@@ -1,40 +1,14 @@
-class App {
-constructor (imageTable){
-  this.logResult = this.logResult.bind(this);
-  this.logError = this.logError.bind(this);
-
-}
-logResult(result) {
-  console.log("Success!", result)
-}
-
-logError(err) {
-  console.error("Failure!", err)
-}
-
-getImage (){
 $.ajax({
   method: "GET",
   url: "https://pixabay.com/api/?key=" + pixabayAPIKey + "&q=yellow+flower&image_type=photo",
   success: this.logResult,
   error: this.logError
 })
+
+function logResult(result) {
+  console.log("Success!", result)
 }
 
-// getLocation(){
-//   $.ajax({
-//     method"GET",
-//     url: https://maps.googleapis.com/maps/api/js
-//     data: {
-//        location: "a location"
-//      }
-//     success: logResult,
-//     error: logError
-//   })
-// }
-
-
-  start() {
-    this.getImage;
-  }
+function logError(err) {
+  console.error("Failure!", err)
 }
